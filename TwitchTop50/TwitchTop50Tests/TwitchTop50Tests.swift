ca3a163a260f317798ce7ad7a865e299afed9887
Rodrigo <rborges.soares@gmail.com>
2015-08-24 20:08:02 -0300

    First commit

---
+++ TwitchTop50/TwitchTop50Tests/TwitchTop50Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TwitchTop50Tests.swift
+//  TwitchTop50Tests
+//
+//  Created by Rodrigo Soares on 24/08/15.
+//  Copyright © 2015 Rodrigo Borges. All rights reserved.
+//
+
+import XCTest
+@testable import TwitchTop50
+
+class TwitchTop50Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
